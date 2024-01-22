4b4f977f2a176a8dfec8c0d00d8639edf3609374
gywns022727 <lifford050227a@gmail.com>
2024-01-22 16:20:36 +0900

    style: header 추가

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Header } from "./style";
+
+export default function index() {
+  return <Header>To Do List</Header>;
+}
---
+++ src/components/Header/style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Header = styled.div`
+  padding: 18px;
+  width: 100%;
+  height: 80px;
+  text-align: center;
+  font-size: 2em;
+  font-weight: 600;
+  color: #4b89dc;
+`;
